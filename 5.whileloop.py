a902c720e3baacd7e25af5f27470035d74703472
Hoki <wahhoki07@gmail.com>
2025-05-28 17:30:06 +0800

    Create 5.whileloop.py

---
+++ 5.whileloop.py
@@ -0,0 +1,14 @@
+# while loop
+
+# while kondisi:
+#    aksi ini
+#    aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang ->{angka}")
+while angka < 10:
+    angka += 1
+    print(f"angka sekaran ->{angka}")
+    print("cukimay")
