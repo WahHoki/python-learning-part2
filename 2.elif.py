2e8fa6d4ef83fea6caaa40d9b5a2398cf54f6951
Hoki <wahhoki07@gmail.com>
2025-05-28 17:27:55 +0800

    Create 2.elif.py

---
+++ 2.elif.py
@@ -0,0 +1,14 @@
+# ELIF = else if statement
+
+nama = input("masukkan nama anda :")
+
+if nama =="ucup":
+    print("kamu ganteng") # aksi true 1
+elif nama == "otong":
+    print("hai otongerss") # aksi true 2
+elif nama == "hoki":
+    print("hai beruntung") # aksi true 3
+else:
+    print("ga kenal!") # aksi false
+
+print("akhir program")
