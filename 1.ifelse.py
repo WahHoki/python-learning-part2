86977e4d7580f39bf29e2f9603649e996da1109c
Hoki <wahhoki07@gmail.com>
2025-05-28 17:23:40 +0800

    Create 1.ifelse.py

---
+++ 1.ifelse.py
@@ -0,0 +1,30 @@
+# if dan else statement
+# 1. if nya
+# 2. kondisi
+# 3. aksinya
+
+nama = input ("siapa nama anda? ")
+
+
+# program sebelumnya
+# id konsisi:m aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="ucup" : print("kamu ganteng habis!!")
+# print("akhir dari program")
+
+# 2. program if indentation
+
+if nama=="ucup":
+    print("kamu ganteng abieszzz!!")
+    print("kamu juga keren banget!")
+print(f"terima kasih {nama}")
+
+
+# 3.  else statement
+if nama =="":
+    print("hai otong, si keren")
+else :
+    print("ah kamu ga keren, kamu bukan otong")
+print("akhir dari program")
